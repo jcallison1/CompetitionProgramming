42b325a1436845bb642504e53f55a92afa9b1bb3
jcallison1 <152124968+jcallison1@users.noreply.github.com>
2024-04-10 23:05:27 -0500

    Unworking mailboxes

---
+++ week13/mailboxes.py
@@ -0,0 +1,27 @@
+import functools
+
+@functools.lru_cache(maxsize=None)
+def dp(start: int, end: int, remaining_boxes: int):
+	if end - start <= 0:
+		return 99999999999999
+	
+	if end - start <= 1:
+		return start
+	
+	if remaining_boxes <= 1:
+		return sum(range(start, end))
+	
+	result = 99999999999999
+	
+	for pivot in range(start, end):
+		result = min(result, max(
+			dp(start, pivot, remaining_boxes),
+			dp(pivot + 1, end, remaining_boxes - 1)
+		))
+	
+	return result
+
+for _ in range(int(input())):
+	boxes, max_crackers = map(int, input().split())
+	
+	print(dp(0, max_crackers + 1, boxes))
